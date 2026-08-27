ceae42fbc5cf2e4240cf20d417a7072cddacb666
Floof Logic Labs <support@flooflogic.com>
2026-08-27 08:25:37 -0500

    formula: let Homebrew infer version

--- Formula/floofclaw.rb
+++ Formula/floofclaw.rb
@@ -2,7 +2,6 @@ class Floofclaw < Formula
   desc "Tiny C runtime for durable AI agents"
   homepage "https://floofclaw.com/"
   url "https://github.com/FloofLogic/Floofclaw/releases/download/v0.30.0/floofclaw-v0.30.0-darwin-arm64.tar.gz"
-  version "0.30.0"
   sha256 "d4baf4ae452778e4f6e30b4902c8e35910d290a51f1cc03d5a2a32c6861734e9"
   license "Apache-2.0"
 
