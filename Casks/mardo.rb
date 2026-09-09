ccb5c093c48d5b5cceaebce3730ffe0cee02f3c7
spawrks <me@spawrks.com>
2026-09-09 11:07:51 -0500

    mardo: update Mardo to 0.9.17

--- Casks/mardo.rb
+++ Casks/mardo.rb
@@ -1,6 +1,6 @@
 cask "mardo" do
-  version "0.9.16"
-  sha256 "8a4e8b9810615ca74a6364a022fe00548532fd042b26432539c206706284098e"
+  version "0.9.17"
+  sha256 "062d093de48ffc2e42433d551116d39597064adc5675830aa1e8af7ab4da7f82"
 
   url "https://github.com/FloofLogic/mardo/releases/download/v#{version}/Mardo.zip"
   name "Mardo"
