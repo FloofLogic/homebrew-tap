e5c90ae5b4c3b69818d7428f1373c02e93fcdefc
spawrks <me@spawrks.com>
2026-09-15 15:29:35 -0500

    mardo: update Mardo to 0.9.23

--- Casks/mardo.rb
+++ Casks/mardo.rb
@@ -19,4 +19,22 @@ cask "mardo" do
 
   app "Mardo.app"
   binary "Mardo.app/Contents/Helpers/mardo", target: "mardo"
+
+  postflight_steps do
+    run "/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister",
+        args: ["-f", "-R", "{{appdir}}/Mardo.app"]
+    run "/usr/bin/pluginkit",
+        args: ["-a", "{{appdir}}/Mardo.app/Contents/PlugIns/MardoPreview.appex"]
+    run "/usr/bin/pluginkit",
+        args: ["-a", "{{appdir}}/Mardo.app/Contents/PlugIns/MardoThumbnail.appex"]
+    run "/usr/bin/pluginkit",
+        args: ["-e", "use", "-i", "com.flooflogic.mardo.QLPreview"]
+    run "/usr/bin/pluginkit",
+        args: ["-e", "use", "-i", "com.flooflogic.mardo.QLThumbnail"]
+    run "/usr/bin/qlmanage", args: ["-r"]
+    run "/usr/bin/qlmanage", args: ["-r", "cache"]
+    terminate_process "QuickLookUIService"
+    terminate_process "quicklookd"
+    terminate_process "com.apple.quicklook.ThumbnailsAgent"
+  end
 end
